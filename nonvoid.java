240383fb4b2b52bcab324e5faa78bdb04c1a950f
manisha4450 <chowdaryc226@gmail.com>
2024-12-11 10:44:30 +0530

    Add files via upload

---
+++ nonvoid.java
@@ -0,0 +1,11 @@
+class nonvoid{
+    public static String s()
+    {
+        return"hello students";
+    }
+    public static void main(String[] args)
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
