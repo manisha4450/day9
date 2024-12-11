3aaeb33141a4b6ec2612c6e519bd2db411ae1a17
manisha4450 <chowdaryc226@gmail.com>
2024-12-11 13:26:30 +0530

    Add files via upload

---
+++ fabanacci2.java
@@ -0,0 +1,14 @@
+class fabanacci2{
+    public static void main(String[] args)
+    {
+        int a=0;
+        int b=1;
+        for(int i=2;i<=10;i++)
+        {
+            int c=a+b;
+            System.out.println(i+"-."+c);
+            a=b;
+            b=c;
+        }
+    }
+}
