3edbddeb42597c48aafbb7b386b428ea6aa2cb38
manisha4450 <chowdaryc226@gmail.com>
2024-12-11 10:48:49 +0530

    Add files via upload

---
+++ nonpublic.java
@@ -0,0 +1,11 @@
+class nonpublic{
+    static String s()
+    {
+        return"hello students";
+    }
+    public static void main(String[] args)
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
