a3d019369f0dc948485e918517c1ba7bbde31cbd
manisha4450 <chowdaryc226@gmail.com>
2024-12-11 15:04:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+    String name;
+    Main(String name)
+    {
+        this.name=name;
+    }
+    void dis1()
+    {
+        System.out.println(name);
+    }
+    void agt()
+    {
+        teacher s=new teacher();
+        s.teach(this);
+    }
+    public static void main(String[] args){
+        Main o=new Main("manisha");
+        o.agt();
+    }
+}
+class teacher{
+    public void teach(Main s){
+        s.dis1();
+    }
+}
